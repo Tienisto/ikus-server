88ed32eb4ef904985a4f0482aa4fb13578cfa232
Tien Do Nam <dev.tien.donam@gmail.com>
2020-08-25 18:01:02 +0200

    fix: simplify SecurityProperties.kt

--- src/main/kotlin/de/ovgu/ikus/properties/SecurityProperties.kt
+++ src/main/kotlin/de/ovgu/ikus/properties/SecurityProperties.kt
@@ -1,12 +1,8 @@
 package de.ovgu.ikus.properties
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ConfigurationProperties(prefix = "security")
-class SecurityProperties {
-    @Autowired
-    lateinit var jwt: JwtProperties
-}
+class SecurityProperties (val jwt: JwtProperties)
