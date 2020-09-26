1dd79150482bc0de0bfb541a0ef0bd0afe0c5b12
Tien Do Nam <dev.tien.donam@gmail.com>
2020-09-26 15:44:12 +0200

    feat(vue): add sameUrl property to link dialog

--- src/main/vue/src/components/dialog/LinkDialog.vue
+++ src/main/vue/src/components/dialog/LinkDialog.vue
@@ -7,21 +7,25 @@
               label="Gruppe"
               v-model="group"
               :items="groups" item-text="name.en" item-value="id"
+              hide-details
               return-object
           ></v-select>
         </v-col>
         <v-col cols="6"></v-col>
         <v-col cols="6">
-          <v-text-field v-model="infoEn" label="Beschreibung (englisch)" :disabled="loading"></v-text-field>
+          <v-text-field v-model="infoEn" label="Beschreibung (englisch)" :disabled="loading" hide-details />
         </v-col>
         <v-col cols="6">
-          <v-text-field v-model="infoDe" label="Beschreibung (deutsch)" :disabled="loading"></v-text-field>
+          <v-text-field v-model="infoDe" label="Beschreibung (deutsch)" :disabled="loading" hide-details />
         </v-col>
         <v-col cols="12">
-          <v-text-field v-model="urlEn" label="Adresse (englisch)" :disabled="loading"></v-text-field>
+          <v-text-field v-model="urlEn" :label="urlEnLabel" placeholder="https://example.com" :disabled="loading" hide-details />
         </v-col>
         <v-col cols="12">
-          <v-text-field v-model="urlDe" label="Adresse (deutsch)" :disabled="loading"></v-text-field>
+          <v-checkbox v-model="sameUrl" label="deutsche Seite = englische Seite" :disabled="loading" hide-details />
+        </v-col>
+        <v-col cols="12">
+          <v-text-field v-show="!sameUrl" v-model="urlDe" label="Adresse (deutsch)" placeholder="https://example.com" :disabled="loading || sameUrl" />
         </v-col>
       </v-row>
     </template>
@@ -40,6 +44,7 @@
 
 <script>
 import GenericDialog from "@/components/dialog/GenericDialog";
+import {showSnackbar} from "@/utils";
 
 export default {
   name: 'LinkDialog',
@@ -68,6 +73,7 @@ export default {
     infoDe: '',
     urlEn: '',
     urlDe: '',
+    sameUrl: true
   }),
   methods: {
     reset: function(group) {
@@ -76,6 +82,7 @@ export default {
       this.infoDe = '';
       this.urlEn = '';
       this.urlDe = '';
+      this.sameUrl = true;
 
       // apply predefined group
       this.group = group;
@@ -86,12 +93,41 @@ export default {
       this.infoDe = link.info.de;
       this.urlEn = link.url.en;
       this.urlDe = link.url.de;
+      this.sameUrl = link.url.en === link.url.de;
     },
     submit: async function() {
+
+      if (!this.infoEn || !this.infoDe) {
+        showSnackbar('Bitte Beschreibungen eingeben');
+        return;
+      }
+
+      if ((this.sameUrl && !this.urlEn) || (!this.sameUrl && (!this.urlEn || !this.urlDe))) {
+        showSnackbar('Bitte Internet-Adresse eingeben');
+        return;
+      }
+
+      if (!this.group.id) {
+        showSnackbar('Bitte Gruppe festlegen');
+        return;
+      }
+
+      const url = this.sameUrl ? { en: this.urlEn, de: this.urlEn } : { en: this.urlEn, de: this.urlDe };
+
+      if (!url.en.startsWith('http://') && !url.en.startsWith('https://')) {
+        showSnackbar('Seite muss mit http:// oder https:// beginnen');
+        return;
+      }
+
+      if (!url.de.startsWith('http://') && !url.de.startsWith('https://')) {
+        showSnackbar('Seite muss mit http:// oder https:// beginnen');
+        return;
+      }
+
       await this.$emit('submit', {
         groupId: this.group.id,
         info: { en: this.infoEn, de: this.infoDe },
-        url: { en: this.urlEn, de: this.urlDe },
+        url,
       });
     }
   },
@@ -99,11 +135,26 @@ export default {
     dialogTitle: function() {
       return this.updating ? 'Link bearbeiten' : 'Neuer Link';
     },
+    urlEnLabel: function() {
+      return this.sameUrl ? 'Adresse' : 'Adresse (englisch)';
+    },
     submitText: function() {
       return this.updating ? 'Speichern' : 'Erstellen';
     },
     submitIcon: function() {
       return this.updating ? 'mdi-content-save' : 'mdi-plus';
+    }
+  },
+  watch: {
+    urlEn: function(newVal) {
+      if (this.sameUrl) {
+        this.urlDe = newVal;
+      }
+    },
+    sameUrl: function(newVal) {
+      if (newVal) {
+        this.urlDe = this.urlEn;
+      }
     }
   }
 }
