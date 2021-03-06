3e37ec26a6cdd273129586676544518916ea3420
Tien Do Nam <dev.tien.donam@gmail.com>
2021-03-06 03:16:54 +0100

    deps: update kotlin to 1.4.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ import java.time.format.DateTimeFormatter
 plugins {
 	id("org.springframework.boot") version "2.4.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.4.30"
-	kotlin("plugin.spring") version "1.4.30"
+	kotlin("jvm") version "1.4.31"
+	kotlin("plugin.spring") version "1.4.31"
 }
 
 group = "de.ovgu"
