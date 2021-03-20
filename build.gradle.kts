8e4972063538bf030f1af7003c55306128e20a22
Tien Do Nam <dev.tien.donam@gmail.com>
2021-03-20 15:28:23 +0100

    deps(spring): update spring boot to 2.4.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 plugins {
-	id("org.springframework.boot") version "2.4.3"
+	id("org.springframework.boot") version "2.4.4"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.4.31"
 	kotlin("plugin.spring") version "1.4.31"
